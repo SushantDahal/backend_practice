3fa6fcaa7bde539491d1ebada0abbea43fa768ca
SushantDahal <SushanDahal246@gmail.com>
2024-01-31 22:24:15 +0545

    adding components

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="w-full my-10">
+      <div className="max-w-[1200px] mx-auto">
+        <h1>Sushant Dahal</h1>
+      </div>
+    </div>
+  );
+};
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import Logo from "../assets/logo.png";
+const Navbar = () => {
+  return (
+    <div className="w-full">
+      <div className="max-w-[1200px] mx-auto bg-[#bcd6e4] h-16 rounded-md my-2 ">
+        <div className="flex justify-between mx-8 items-center  py-3">
+          <img src={Logo} alt="/" className="w-[150px]  mx-4" />
+          <ul className="md:flex hidden uppercase font-semibold cursor-pointer">
+            <li className="mx-6">Dashboard</li>
+            <li className="mx-6">Sign Up</li>
+            <li className="mx-6">Sign IN</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
